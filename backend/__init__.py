8cb68891102f2adbef24bae9f400753f1af73ce5
Dhananjana Nadee Kumari <dhananjananadeekumari@gmail.com>
2025-10-14 15:34:42 +0530

    Revert "Merge pull request #6 from shoppingPlanners/Rating_Calculator"
    This reverts commit 48bd28a7651ec3eb5f7d2364421eceec5d0a512d, reversing
    changes made to ed37eeff547545da82ff6e6bc643de0c997588fe.

--- backend/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Sustainable Shopping Planner Backend Package
-"""
-
-__version__ = "1.0.0"
-__author__ = "Sustainable Shopping Planner Team"
-__description__ = "Python FastAPI backend with MongoDB and AI rating system"
